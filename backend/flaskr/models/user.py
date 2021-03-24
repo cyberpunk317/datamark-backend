357ef0af308f028e0745c5fe3d10d56062454931
Bohdan <60684271+BohdanMogu4iy@users.noreply.github.com>
2021-03-24 14:54:03 +0200

    Change structure (#4)
    * added action with pytest unittesting
    
    * Create main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update actions
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * removed templates
    
    * added basic user API
    
    * added postgres configs
    
    * added basic mongo config
    
    * added flask app script
    
    * minor updates
    
    * added require
    
    * minor updates to User entity
    
    * added Dockerfile for API
    
    * submodule added
    
    * Client dev module
    
    * fixed folder in main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    Co-authored-by: kirill.zajcev <kirill.zajcev@murka.com>
    Co-authored-by: cyberpunk317 <44346535+cyberpunk317@users.noreply.github.com>

---
+++ backend/flaskr/models/user.py
@@ -0,0 +1,15 @@
+from config import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    registration_date = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=True)
+    name = db.Column(db.String(15), nullable=True)
+    password = db.Column(db.String(128), nullable=True)
+    num_mark_tasks = db.Column(db.Integer, nullable=True)
+    last_activity_ds = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=True)
+
+    def __repr__(self):
+        return f'<User: {self.name}>'
